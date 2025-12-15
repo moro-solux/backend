e5e4853cc94321856d4c6592cf104ac567147baf
SangHee-Hong <hongdabagi@gmail.com>
2025-12-15 12:43:55 +0900

    Chore: 도메인 중신 패키지 구조 세팅

---
+++ src/test/java/com/example/moro/MoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
