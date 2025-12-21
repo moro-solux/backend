5778ba04fe17f3e0e15a87d1e6f559eba5ea8f0a
jimin <vortexjml@sookmyung.ac.kr>
2025-12-21 18:44:06 +0900

    swagger에 authorize 버튼 추가

--- src/main/java/com/example/moro/global/config/SwaggerConfig.java
+++ src/main/java/com/example/moro/global/config/SwaggerConfig.java
@@ -2,11 +2,13 @@ package com.example.moro.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.models.info.Contact;
-
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,7 +29,15 @@ public class SwaggerConfig {
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("Local Server")
-                ));
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요")))
+                .security(List.of(new SecurityRequirement().addList("bearerAuth")));
     }
     @Bean
     public ObjectMapper objectMapper() {
