ffe59e96e1202ff59173fb29d4c9e39245f761ba
이수 <149984963+suuu004@users.noreply.github.com>
2025-12-29 19:45:21 +0900

    Colormap 주키 설정 추가

--- src/main/java/com/example/moro/app/colormap/entity/ColorMap.java
+++ src/main/java/com/example/moro/app/colormap/entity/ColorMap.java
@@ -1,9 +1,14 @@
 package com.example.moro.app.colormap.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class ColorMap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 자동으로 번호를 매김
     private Long colorId;
     private String colorTheme;
     private String hexCode;
