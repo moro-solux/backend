8adb662934f5c5ab3ecc2681f7c571e6bdf89ce4
SangHee-Hong <hongdabagi@gmail.com>
2025-12-31 02:01:01 +0900

    feat: usercolormaprepository 대표색상 목록 초기화 및 해금 목록 조회 메서드 구현

--- src/main/java/com/example/moro/app/colormap/repository/UserColorMapRepository.java
+++ src/main/java/com/example/moro/app/colormap/repository/UserColorMapRepository.java
@@ -4,6 +4,9 @@ import com.example.moro.app.colormap.entity.ColorMap;
 import com.example.moro.app.colormap.entity.UserColorMap;
 import com.example.moro.app.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +19,18 @@ public interface UserColorMapRepository extends JpaRepository<UserColorMap, Long
     List<UserColorMap> findByMemberAndIsRepresentativeTrue(Member member);
 
     Optional<UserColorMap> findByMemberIdAndColorMapColorId(Long memberId, Long colorId);
+
+    @Modifying
+    @Query("""
+    update UserColorMap u
+    set u.isRepresentative = false
+    where u.member = :member
+      and u.isRepresentative = true
+    """)
+    void clearRepresentativeByMember(@Param("member") Member member);
+
+    List<UserColorMap> findByMemberAndUnlockedIsTrueAndColorMap_ColorIdIn(Member member, List<Long> colorIds);
+
+
 
 }
