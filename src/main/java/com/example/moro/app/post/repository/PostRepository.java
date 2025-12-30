0905d1b67ae5e0352c4789a9b15342e3a2e61d32
SangHee-Hong <hongdabagi@gmail.com>
2025-12-30 21:46:35 +0900

    feat: postrepository 색상 옵션별 post 조회 메서드 구현

--- src/main/java/com/example/moro/app/post/repository/PostRepository.java
+++ src/main/java/com/example/moro/app/post/repository/PostRepository.java
@@ -1,11 +1,22 @@
 package com.example.moro.app.post.repository;
 
+import com.example.moro.app.member.entity.Member;
 import com.example.moro.app.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
     int countByMemberId(Long memberId);
 
+    Page<Post> findByMemberOrderByCreatedAtDesc(Member member, Pageable pageable);
+
+    Page<Post> findByMemberAndMainColorIdInOrderByCreatedAtDesc(Member member, List<Integer> colorIds, Pageable pageable);
+
+    Page<Post> findByMemberAndMainColorIdOrderByCreatedAtDesc(Member member, Integer colorId, Pageable pageable);
 }
