27e5656c8b135f882e2fea38a4f2f3f6f5e70d8e
이수 <149984963+suuu004@users.noreply.github.com>
2025-12-31 11:58:12 +0900

    레파지토리 수정

--- src/main/java/com/example/moro/app/colormap/repository/UserColorMapRepository.java
+++ src/main/java/com/example/moro/app/colormap/repository/UserColorMapRepository.java
@@ -2,6 +2,7 @@ package com.example.moro.app.colormap.repository;
 
 import com.example.moro.app.colormap.entity.ColorMap;
 import com.example.moro.app.colormap.entity.UserColorMap;
+import com.example.moro.app.colormap.entity.UserColorMapId;
 import com.example.moro.app.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,25 +12,23 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserColorMapRepository extends JpaRepository<UserColorMap, Long> {
+public interface UserColorMapRepository extends JpaRepository<UserColorMap, UserColorMapId> {
+
     boolean existsByMemberAndColorMap(Member member, ColorMap colorMap);
-
     int countByMemberAndUnlockedTrue(Member member);
-
     List<UserColorMap> findByMemberAndIsRepresentativeTrue(Member member);
-
     Optional<UserColorMap> findByMemberIdAndColorMapColorId(Long memberId, Long colorId);
 
+    // 2. 대표색 초기화
     @Modifying
-    @Query("""
-    update UserColorMap u
-    set u.isRepresentative = false
-    where u.member = :member
-      and u.isRepresentative = true
-    """)
+    @Query("update UserColorMap u set u.isRepresentative = false\n" +
+            "    where u.member = :member\n" +
+            "      and u.isRepresentative = true")
     void clearRepresentativeByMember(@Param("member") Member member);
 
-    List<UserColorMap> findByMemberAndUnlockedIsTrueAndColorMap_ColorIdIn(Member member, List<Long> colorIds);
+    // 3. 통합 조회
+    @Query("SELECT ucm FROM UserColorMap ucm JOIN FETCH ucm.colorMap WHERE ucm.member.id = :memberId")
+    List<UserColorMap> findAllByMemberIdWithColorMap(@Param("memberid") Long memberId);
 
 
 
