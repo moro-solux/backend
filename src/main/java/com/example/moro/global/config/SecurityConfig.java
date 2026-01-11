a916cd9fd9a878e0b5b66de1ba23448cb3df7598
jimin <vortexjml@sookmyung.ac.kr>
2026-01-11 14:02:34 +0900

    Fix; 게시물 api PATCH 안됨 해결

--- src/main/java/com/example/moro/global/config/SecurityConfig.java
+++ src/main/java/com/example/moro/global/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","PATCH","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
